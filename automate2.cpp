f15dcd55996c0c27330affbc7ceb8d74a5fccb16
Adrien <adrienthuau@gmail.com>
2018-05-02 11:49:22 +0200

    Tableaux pour simplification du code

---
+++ automate2.cpp
@@ -0,0 +1,6 @@
+#include "automate2.h"
+
+Automate2::Automate2()
+{
+
+}
---
+++ automate2.h
@@ -0,0 +1,13 @@
+#ifndef AUTOMATE2_H
+#define AUTOMATE2_H
+
+#include <QObject>
+#include <QWidget>
+
+class Automate2
+{
+public:
+    Automate2();
+};
+
+#endif // AUTOMATE2_H
